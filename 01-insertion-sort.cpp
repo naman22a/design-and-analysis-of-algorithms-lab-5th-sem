c8eeef6ff87c3b92b2b03f6840ecabafd33d803c
Naman Arora <namanarora1022@gmail.com>
2025-08-04 14:57:21 +0530

    added maam wala code also

--- 01-insertion-sort.cpp
+++ 01-insertion-sort.cpp
@@ -21,13 +21,28 @@ void insertion_sort(int arr[], int n)
     }
 }
 
+void insertion_sort2(int arr[], int n)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        int key = arr[j];
+        int i = j - 1;
+        while (i >= 0 && arr[i] > key)
+        {
+            arr[i + 1] = arr[i];
+            i = i - 1;
+        }
+        arr[i + 1] = key;
+    }
+}
+
 int main()
 {
     int arr[] = {5, 4, 3, 2, 1};
     int n = 5;
 
     print_array(arr, n);
-    insertion_sort(arr, n);
+    insertion_sort2(arr, n);
     print_array(arr, n);
 
     return 0;
