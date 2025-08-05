2f50030c08c73858d7bd028c5196bef8a32435b1
Naman Arora <namanarora1022@gmail.com>
2025-08-05 17:53:20 +0530

    refactor: take input from user

--- 01-insertion-sort.cpp
+++ 01-insertion-sort.cpp
@@ -23,29 +23,21 @@ void insertion_sort(int arr[], int n)
     }
 }
 
-void insertion_sort2(int arr[], int n)
-{
-    for (int j = 1; j <= n - 1; j++)
-    {
-        int key = arr[j];
-        int i = j - 1;
-        while (i >= 0 && arr[i] > key)
-        {
-            arr[i + 1] = arr[i];
-            i = i - 1;
-        }
-        arr[i + 1] = key;
-    }
-}
-
 int main()
 {
-    int arr[] = {5, 4, 3, 2, 1};
-    int n = 5;
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
     print_array(arr, n);
-    insertion_sort2(arr, n);
+    insertion_sort(arr, n);
     print_array(arr, n);
+
+    delete[] arr;
 
     return 0;
 }
